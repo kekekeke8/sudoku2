3df0e226ed83b0034a4bdb435898c585077da17a
lirenzhao@gmail.com <lirenzhao@gmail.com@951a5375-5e1e-0473-a69c-c0ad9e2761ec>
2011-01-03 14:12:24 +0000

    功能完成，下面要修改界面和数据更新了

--- trunk/Sudoku/src/com/liren/sudoku/model/SQLHelper.java
+++ trunk/Sudoku/src/com/liren/sudoku/model/SQLHelper.java
@@ -1,6 +1,5 @@
 package com.liren.sudoku.model;
 
-
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -9,7 +8,7 @@ import android.util.Log;
 
 public class SQLHelper extends android.database.sqlite.SQLiteOpenHelper {
 	private final static String DATABASE_NAME = "sudoku";
-	private final static int 	DATABASE_VERSION = 2;
+	private final static int DATABASE_VERSION = 2;
 	private final static String TABLE_NAME = "sudoku";
 	private final static String FIELD_ID = "ID";
 	private final static String FIELD_LEVEL = "Level";
@@ -19,6 +18,54 @@ public class SQLHelper extends android.database.sqlite.SQLiteOpenHelper {
 	private final static String FIELD_ERROR = "Error";
 	private final static String FIELD_FINISH = "Finish";
 	private final static String FIELD_LASTDATE = "LastDate";
+	private final static String[] fields = { FIELD_ID, FIELD_LEVEL, FIELD_DATA,
+			FIELD_TIMECOST, FIELD_TIPCOUNT, FIELD_ERROR, FIELD_FINISH };
+	private final static String[] datas = { 
+		"11,71,60,41,51,80,31,90,21," +
+		"90,80,31,20,60,71,50,11,40," +
+		"41,51,21,90,31,10,70,61,81," +
+		"61,41,81,70,10,51,21,31,90," +
+		"50,91,70,30,21,61,40,80,11," +
+		"20,30,11,81,91,41,61,51,71," +
+		"81,20,50,60,70,91,10,41,30," +
+		"31,11,40,51,81,21,91,71,60," +
+		"71,61,91,11,40,31,80,21,50" ,		
+		"30,70,80,60,40,90,51,10,21," +
+		"60,51,21,70,80,10,30,91,40," +
+		"90,10,40,21,31,51,60,70,81," +
+		"80,30,61,40,10,20,91,50,70," +
+		"20,40,71,51,91,61,10,80,30," +
+		"50,90,10,30,71,80,21,40,60," +
+		"10,20,30,81,51,41,70,61,90," +
+		"40,61,50,90,20,70,81,31,10," +
+		"71,80,91,10,60,30,40,20,50" ,		
+		"30,70,80,60,40,90,51,10,21," +
+		"60,51,21,70,80,10,30,91,40," +
+		"90,10,40,21,31,51,60,70,81," +
+		"80,30,61,40,10,20,91,50,70," +
+		"20,40,71,51,91,61,10,80,30," +
+		"50,90,10,30,71,80,21,40,60," +
+		"10,20,30,81,51,41,70,61,90," +
+		"40,61,50,90,20,70,81,31,10," +
+		"71,80,91,10,60,30,40,20,50" ,		
+		"30,70,80,60,40,90,51,10,21," +
+		"60,51,21,70,80,10,30,91,40," +
+		"90,10,40,21,31,51,60,70,81," +
+		"80,30,61,40,10,20,91,50,70," +
+		"20,40,71,51,91,61,10,80,30," +
+		"50,90,10,30,71,80,21,40,60," +
+		"10,20,30,81,51,41,70,61,90," +
+		"40,61,50,90,20,70,81,31,10," +
+		"71,80,91,10,60,30,40,20,50" ,		
+		"30,70,80,60,40,90,51,10,21," +
+		"60,51,21,70,80,10,30,91,40," +
+		"90,10,40,21,31,51,60,70,81," +
+		"80,30,61,40,10,20,91,50,70," +
+		"20,40,71,51,91,61,10,80,30," +
+		"50,90,10,30,71,80,21,40,60," +
+		"10,20,30,81,51,41,70,61,90," +
+		"40,61,50,90,20,70,81,31,10," +
+		"71,80,91,10,60,30,40,20,50"};
 
 	public SQLHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -27,25 +74,29 @@ public class SQLHelper extends android.database.sqlite.SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		String sql = "Create table %s (%s integer primary key autoincrement,%s integer,%s text,%s bigint,%s int,%s int,%s int,%s datetime);";
-		sql = String.format(sql, TABLE_NAME, FIELD_ID, FIELD_LEVEL,FIELD_DATA,FIELD_TIMECOST,FIELD_TIPCOUNT,FIELD_ERROR,FIELD_FINISH,FIELD_LASTDATE);
+		sql = String.format(sql, TABLE_NAME, FIELD_ID, FIELD_LEVEL, FIELD_DATA,
+				FIELD_TIMECOST, FIELD_TIPCOUNT, FIELD_ERROR, FIELD_FINISH,
+				FIELD_LASTDATE);
 		db.execSQL(sql);
-		
-		InitData( db);
-		Log.d("D","Database Create finished.sql:" + sql);
+
+		InitData(db);
+		Log.d("D", "Database Create finished.sql:" + sql);
 	}
 
 	private void InitData(SQLiteDatabase db) {
-		String data = "11,71,60,41,51,80,31,90,21,90,80,31,20,60,71,50,11,40,41,51,21,90,31,10,70,61,81,61,41,81,70,10,51,21,31,90,50,91,70,30,21,61,40,80,11,20,30,11,81,91,41,61,51,71,81,20,50,60,70,91,10,41,30,31,11,40,51,81,21,91,71,60,71,61,91,11,40,31,80,21,50";
-		ContentValues cv = new ContentValues();
-		cv.put(FIELD_LEVEL, 1);
-		cv.put(FIELD_DATA, data);
-		cv.put(FIELD_TIMECOST, 0);
-		cv.put(FIELD_TIPCOUNT, 3);
-		cv.put(FIELD_ERROR, 0);
-		cv.put(FIELD_FINISH, 0);
-		cv.put(FIELD_LASTDATE, 0);
-		long row = db.insert(TABLE_NAME, null, cv);
-		Log.d("D","insert data finished. ret=" + row);
+		for (int i = 0; i < datas.length; i++) {
+			String data = datas[i];
+			ContentValues cv = new ContentValues();
+			cv.put(FIELD_LEVEL, i + 1);
+			cv.put(FIELD_DATA, data);
+			cv.put(FIELD_TIMECOST, 0);
+			cv.put(FIELD_TIPCOUNT, 3);
+			cv.put(FIELD_ERROR, 0);
+			cv.put(FIELD_FINISH, 0);
+			cv.put(FIELD_LASTDATE, 0);
+			long row = db.insert(TABLE_NAME, null, cv);
+			Log.d("D", "insert data finished. ret=" + row);
+		}
 	}
 
 	@Override
@@ -57,39 +108,60 @@ public class SQLHelper extends android.database.sqlite.SQLiteOpenHelper {
 
 	public Cursor select() {
 		SQLiteDatabase db = this.getReadableDatabase();
-		String[] fields = {FIELD_ID,FIELD_LEVEL,FIELD_DATA,FIELD_TIMECOST,FIELD_TIPCOUNT,FIELD_ERROR,FIELD_FINISH};
-		Cursor cursor = db.query(TABLE_NAME, fields, null, null, null, null,FIELD_ID);
-		return cursor;
-	}
-	
-	//Query new sudoku in this level
-	public Cursor select(int level){
-		SQLiteDatabase db = this.getReadableDatabase();
-		String[] fields = {FIELD_ID,FIELD_LEVEL,FIELD_DATA,FIELD_TIMECOST,FIELD_TIPCOUNT,FIELD_ERROR,FIELD_FINISH};
-		String where = FIELD_LEVEL + "= ?" + " AND " + FIELD_FINISH + "= ?";
-		String[] wherevalue = { Integer.toString(level) ,"0"};
-		Cursor cursor = db.query(TABLE_NAME, fields, where, wherevalue, null, null,
+		String[] fields = { FIELD_ID, FIELD_LEVEL, FIELD_DATA, FIELD_TIMECOST,
+				FIELD_TIPCOUNT, FIELD_ERROR, FIELD_FINISH };
+		Cursor cursor = db.query(TABLE_NAME, fields, null, null, null, null,
 				FIELD_ID);
 		return cursor;
 	}
 
-	public SudokuModel play(int level){		
-		Cursor c = this.select();
-		while (c.moveToFirst()) {
-			com.liren.sudoku.model.SudokuModel s = new com.liren.sudoku.model.SudokuModel();
-			s.id = c.getInt(0);
-			s.level = c.getInt(1);
-			s.data = c.getString(2);
-			s.timecost = c.getInt(3);
-			s.tipcount = c.getInt(4);
-			s.error = c.getInt(5);
-			s.finish = c.getInt(6);			
-			return s;
+	// Query new sudoku in this level
+	// new game in this level
+	public Cursor select(int level) {
+		SQLiteDatabase db = this.getReadableDatabase();
+		String where = FIELD_LEVEL + "= ?" + " AND " + FIELD_FINISH + "= ?";
+		String[] wherevalue = { Integer.toString(level), "0" };
+		Cursor cursor = db.query(TABLE_NAME, fields, where, wherevalue, null,
+				null, FIELD_ID);
+		return cursor;
+	}
+
+	public SudokuModel loadGame(int level) {
+		SQLiteDatabase db = this.getReadableDatabase();
+		String where = FIELD_LEVEL + "= ?" + " AND " + FIELD_FINISH + "= ?";
+		String[] wherevalue = { Integer.toString(level), "0" };
+		Cursor cursor = db.query(TABLE_NAME, fields, where, wherevalue, null,
+				null, FIELD_ID);
+		return ParseSudokuModel(cursor);
+	}
+
+	private SudokuModel ParseSudokuModel(Cursor cursor) {
+		while (cursor.moveToFirst()) {
+			com.liren.sudoku.model.SudokuModel model = new com.liren.sudoku.model.SudokuModel();
+			model.id = cursor.getInt(0);
+			model.level = cursor.getInt(1);
+			model.data = cursor.getString(2);
+			model.timecost = cursor.getInt(3);
+			model.tipcount = cursor.getInt(4);
+			model.error = cursor.getInt(5);
+			model.finish = cursor.getInt(6);
+			return model;
 		}
 		return null;
-	} 
-	
-	public long insert(String data,int level,int tip) {
+	}
+
+	public SudokuModel loadResumeGame() {
+		SQLiteDatabase db = this.getReadableDatabase();
+		String[] fields = { FIELD_ID, FIELD_LEVEL, FIELD_DATA, FIELD_TIMECOST,
+				FIELD_TIPCOUNT, FIELD_ERROR, FIELD_FINISH };
+		String where = FIELD_FINISH + "= ?";
+		String[] wherevalue = { "1" };
+		Cursor cursor = db.query(TABLE_NAME, fields, where, wherevalue, null,
+				null, FIELD_ID);
+		return ParseSudokuModel(cursor);
+	}
+
+	public long insert(String data, int level, int tip) {
 		SQLiteDatabase db = this.getWritableDatabase();
 		ContentValues cv = new ContentValues();
 		cv.put(FIELD_LEVEL, level);
@@ -100,7 +172,7 @@ public class SQLHelper extends android.database.sqlite.SQLiteOpenHelper {
 		cv.put(FIELD_FINISH, 0);
 		cv.put(FIELD_LASTDATE, 0);
 		long row = db.insert(TABLE_NAME, null, cv);
-		Log.d("D","insert data finished. ret=" + row);
+		Log.d("D", "insert data finished. ret=" + row);
 		return row;
 	}
 
@@ -111,12 +183,24 @@ public class SQLHelper extends android.database.sqlite.SQLiteOpenHelper {
 		db.delete(TABLE_NAME, where, wherevalue);
 	}
 
-	public void update(SudokuModel sudoku){
-		if(sudoku != null){
-			update(sudoku.id,sudoku.timecost,sudoku.tipcount,sudoku.error,sudoku.finish,sudoku.data);
+	public void update(SudokuModel sudoku) {
+		if (sudoku != null) {
+			update(sudoku.id, sudoku.timecost, sudoku.tipcount, sudoku.error,
+					sudoku.finish, sudoku.data);
 		}
 	}
- 	public void update(int id, Long timecost,int tipcount,int error,int finish,String data) {
+
+	public void abandonGame(){
+		SQLiteDatabase db = this.getWritableDatabase();
+		String where = FIELD_FINISH + "= ?";
+		String[] wherevalue = { Integer.toString(1) };
+		ContentValues cv = new ContentValues();
+		cv.put(FIELD_FINISH, 0);
+		db.update(TABLE_NAME, cv, where, wherevalue);
+	}
+	
+	public void update(int id, Long timecost, int tipcount, int error,
+			int finish, String data) {
 		SQLiteDatabase db = this.getWritableDatabase();
 		String where = FIELD_ID + "= ?";
 		String[] wherevalue = { Integer.toString(id) };
---
+++ trunk/Sudoku/src/com/liren/sudoku/model/UpdateData.java
@@ -0,0 +1,5 @@
+package com.liren.sudoku.model;
+
+public class UpdateData {
+	private final static String url= "http://1.shuoshuo.sinaapp.com/sudoku.xml";
+}
